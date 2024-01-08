0bf0bc005eecdd55a5fcbe7a3e3ae303e9ce7fb8
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2024-01-08 01:41:06 +0000

    ejercicio

--- src/app.py
+++ src/app.py
@@ -37,15 +37,102 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    results =[]
+    users = User.query.all()
+    for user in users:
+         results.append(user.serialize())
+    return jsonify(results), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user', methods=['GET'])
+def get_person(person_id):
+    person = People.query.get ( person_id)
 
-    return jsonify(response_body), 200
+    if person:
+        return jsonify(person.serialize()), 200
+    
+    return jsonify({'message': 'Person not found'}), 404
+
+
+
+#---fAVS
+def get_all_favorites (user_id):
+    User = User.query.get ( user_id)
+
+    serialize_favorites = []
+    for favorite in User.favorites:
+        serialize_favorites.append(favorite.serialize())
+
+    if len (serialize_favorites) > 0:
+        return jsonify (serialize_favorites), 200
+    
+    return jsonify({'message': 'Cannot find favorite'}), 404
+
+
+#---ADD PLANETS
+@app.route('/planets', methods=['POST'])
+def add_planet():
+    req_body = request.get_json()
+    person_name = req_body ['name']
+    person_mass = req_body ['mass']
+    person_environment = req_body ['environment']
+
+    new_planet = People (name = person_name, mass = person_mass, environment = person_environment)
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify ({'Message': 'Posted new planet to database'}), 200
+
+#---GET PLANETS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    
+    planets = Planets.query.all ()
+    serialized_planets = []
+    for planet in planets:
+        serialized_planets.append(planet.serialize())
+
+    if len(serialized_planets) > 0:
+        return jsonify(serialized_planets), 200
+    
+    return jsonify ({'Message': 'No planets in database'}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+# gets a certain planet ID info
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get ( planet_id)
+
+    if planet:
+        return jsonify(planet.serialize()), 200
+    
+    return jsonify({'message': 'Planet not found'}), 404
+
+
+
+# add a new favorite planet to the current user 
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet (planet_id):
+    user_id = 1
+
+    current_user = User.query.get(user_id)
+
+    if current_user: 
+        planet = Planet.query.get(planet_id)
+
+        favorites = Favorites ()
+        favorites.user = current_user
+        favorites.plamet = planet
+
+        current_user.favorites.append(favorites)
+        db.session.commit()
+
+        return jsonify({ 'Message': 'Added planet to user favorites'}), 200
+    
+    return jsonify({"Message": "Error while Adding planet"}), 404
--- src/models.py
+++ src/models.py
@@ -2,11 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column (db.String (20),unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +19,47 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
+
+class Favorite (db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column (db.Integer, primary_key=True)
+    user_id = db.Column (db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column (db.Integer,db.ForeignKey('planets.id'),nullable=True)
+    people_id = db.Column (db.Integer,db.ForeignKey('people.id'),nullable=True)
+
+
+class People (db.Model):
+    __tablename__ = 'people'
+    id = db.Column (db.Integer, primary_key=True)
+    name = db.Column (db.String(100)) # does it need unique?
+    age = db.Column (db.Integer)
+    height = db.Column (db.Integer)
+    
+
+    def serialize (self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "height": self.height
+        }
+    
+class Planets (db.Model):
+    __tablename__ = 'planets'
+    id = db.Column (db.Integer, primary_key=True)
+    name = db.Column (db.String(100)) # does it need unique?
+    mass = db.Column (db.Integer)
+    environment = db.Column (db.String (100))
+
+    def serialize (self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "mass": self.mass,
+            "environment": self.environment
+        }
+    
