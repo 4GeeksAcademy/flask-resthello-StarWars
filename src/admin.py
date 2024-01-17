86074de7881d0fa35cdbec386fc43741d00acb86
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2024-01-17 19:02:03 +0000

    admin correction

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Star_Systems, Planets, Species, Characters, Factions, User_Favorite
+from models import db, User, Item,  Character, Vehicle, Characters, Planet, User_Favorite
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -10,11 +10,11 @@ def setup_admin(app):
 
     # Add your models here, for example, this is how we add the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Star_Systems, db.session))
-    admin.add_view(ModelView(Planets, db.session))
-    admin.add_view(ModelView(Species, db.session))
-    admin.add_view(ModelView(Characters, db.session))
-    admin.add_view(ModelView(Factions, db.session))
+    admin.add_view(ModelView(Item, db.session))
+    admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Vehicle, db.session))
     admin.add_view(ModelView(User_Favorite, db.session))
+    admin.add_view(ModelView(Characters, db.session))
     # You can duplicate that line to add new models
     # admin.add_view(ModelView(YourModelName, db.session))
